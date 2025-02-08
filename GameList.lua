2d905368776dc2a88bcbded7d5ef80e7e9cabd06
wpisstestfprg <nguyenthud01uu@gmail.com>
2025-02-08 08:40:41 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -2,6 +2,6 @@ local Games = {
   [2753915549] = "https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoNewUpdated.luau", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoNewUpdated.luau", -- Blox Fruit Sea 2
   [7449423635] = "https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/VolcanoNewUpdated.luau", -- Blox Fruit Sea 3
-  [18668065416] = = "https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/BlueLockLuau"
+  [18668065416] = "https://raw.githubusercontent.com/wpisstestfprg/Volcano/refs/heads/main/BlueLockLuau"
 }
 return Games
